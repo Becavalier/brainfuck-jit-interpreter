f969996bf193bfe40e9f8f7093b7b716b4fcd4e6
Becavalier <yhorg@hotmail.com>
2021-05-19 00:01:49 +0800

    Code formatting.

--- interpreter.cc
+++ interpreter.cc
@@ -32,7 +32,8 @@ uint8_t* allocateExecMem(size_t size) {
     mmap(
       NULL,
       size, 
-      PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 
+      PROT_READ | PROT_WRITE | PROT_EXEC, 
+      MAP_PRIVATE | MAP_ANONYMOUS, 
       -1,
       0));
 }
